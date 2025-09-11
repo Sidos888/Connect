f679714b4ebeca64420cf0e94cd0e17f6beaa282
Sid Farquharson <sidfarquharson@gmail.com>
2025-09-11 11:49:57 +0930

    Fix broken Connect references in iOS project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.sidfarquharson.connect',
   appName: 'Connect',
-  webDir: 'out',
-  ios: {
-    scheme: 'Connect'
-  }
+  webDir: 'out'
 };
 
 export default config;
